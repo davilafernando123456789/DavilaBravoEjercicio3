2a2653a80daf44b561012a3ce275868f1434bda6
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2023-10-07 22:24:01 -0500

    Subiendo laboratorio 5

---
+++ Ejercicio3/Ejercicio3/Ejercicio3/ImageResourceExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio3
+{
+    public class ImageResourceExtension : IMarkupExtension
+
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+
+            if (Source == null)
+                return null;
+            //Do your translation lookup here, using whatever method you
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
